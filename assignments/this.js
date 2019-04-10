c55008d5ebbd2daffd7421716e7d9369dc283782
Cory Hedeen <hedeencory@gmail.com>
2019-04-10 15:36:22 -0400

    finished questions about this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window: in the global scope, "this" will reference, or bind to, the window.
+* 2. Implicit Binding: In the local scope, "this" will refer to the object in which it is contained.
+* 3. New Binding: A constructor object creates a template in which "this" refers to the object. Consequently, "this" will refer to any object formed from this template.
+* 4. Explicit Binding: When the function is called, "this" is manually set to the object of choice.
 *
 * write out a code example of each explanation above
 */
